bdaf39a894e35a3e1a31c7424fc890024ee7f742
Fast <patelrahil1210@gmail.com>
2025-09-05 11:41:10 -0400

    initial push

---
+++ include/MathUtils.h
@@ -0,0 +1,8 @@
+#ifndef MATHUTILS_H
+#def MATHUTILS_H
+
+namespace MathUtils {
+int add( int a, int b);
+int multiply( int a, int b);
+}
+#endif
---
+++ src/AdvancedMath.cpp
@@ -0,0 +1,10 @@
+#include "AdvancedMath.h"
+#include "MathUtils.h"
+
+namespace AdvancedMath {
+    int square(int n){
+	
+        return MathUtils::multiply(n,n);
+}
+
+}
